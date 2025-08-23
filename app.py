5a4b3c28d1b873c0068e93bb3884307603f27556
lpfgarcia <lpfgarcia@gmail.com>
2025-08-23 19:57:31 -0300

    include area, general and specific hierarchy

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import os
+import protocol
 import chainlit as cl
 
 from openai import AsyncOpenAI
 
 MODEL_NAME = 'gpt-4o'
 OPENAI_KEY = os.getenv('OPENAI_API_KEY')
-
-ADULT = '''Protocolo 1 – Distúrbios de Refração ou Acomodação\n
-Protocolo 2 – Catarata\n
-Protocolo 3 – Retinopatia ou Outras Doenças de Retina\n
-Protocolo 4 – Estrabismo\n
-Protocolo 5 – Doenças das Pálpebras, Vias Lacrimais e Órbita\n
-Protocolo 6 – Glaucoma\n
-Protocolo 7 – Doenças da Córnea e da Superfície Ocular\n
-Protocolo 8 – Uveítes\n'''
 
 def read_file(path):
     with open(path, 'r', encoding='utf-8') as f:
@@ -24,8 +16,8 @@ def read_file(path):
 async def start():
     client = AsyncOpenAI(api_key=OPENAI_KEY)
     cl.user_session.set('client', client)
-    await cl.Message(content='Olá! Sou um Assistente da Secretaria de Saúde do Distrito Federal.\n' + 
-        'Por favor, informe a idade, sexo, justificativa, risco e procedimento solicitado do paciente.').send()
+    await cl.Message(content=f'''Olá! Sou um Assistente da Secretaria de Saúde do Distrito Federal.
+        Por favor, informe a idade, o sexo, a justificativa, o risco e o procedimento solicitado do paciente.''').send()
 
 @cl.step(name='Conferindo se as informações estão completas...', type='tool')
 async def info_checker(msg: cl.Message):
@@ -34,8 +26,9 @@ async def info_checker(msg: cl.Message):
     response = await client.chat.completions.create(
         model=MODEL_NAME, 
         messages=[
-            {'content': f'Você é um Assistente da Secretaria de Saúde do Distrito Federal.', 'role': 'system'},
-            {'content': f'No contexto da mensagem, o usuário informou a idade, o sexo, a justificativa, o risco e o procedimento solicitado?\nResponda **somente** **SIM** ou **NÃO**.\n\n Mensagem: {msg.content}', 'role': 'user'}
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''O usuário informou a idade, o sexo, a justificativa, o risco e o procedimento solicitado na mensagem?
+            Responda **somente** com **SIM** ou **NÃO**. Mensagem: {msg.content}''', 'role': 'user'}
         ], temperature=0
     )
 
@@ -45,50 +38,89 @@ async def info_checker(msg: cl.Message):
         return True
     return False
 
-@cl.step(name='Verificando se é Oftalmologia Pediátrica ou Adulto...', type='tool')
-async def type_extractor(msg: cl.Message):
-
+@cl.step(name='Verificando quais informações estão incompletas...', type='tool')
+async def info_explanation(msg: cl.Message):
+    
     client = cl.user_session.get('client')
     response = await client.chat.completions.create(
         model=MODEL_NAME, 
         messages=[
-            {'content': f'Você é um Assistente da Secretaria de Saúde do Distrito Federal.', 'role': 'system'},
-            {'content': f'Com base no contexto da mensagem, qual dos protocolos (Oftalmologia Adulto ou Oftalmologia Pediátrica) deve ser utilizado?\nResponda **somente** **adulto** ou **pediátrica**.\n\n Mensagem: {msg.content}', 'role': 'user'}
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''O usuário esqueceu de informar um ou mais dos seguintes campos: idade, sexo, justificativa, risco e procedimento solicitado.
+            Indique **somente** quais campos estão faltando na mensagem, sem acrescentar explicações extras. 
+            Mensagem: {msg.content}''', 'role': 'user'}
         ], temperature=0
     )
 
     response = response.choices[0].message.content
+    return response
 
-    if 'pediátrica' in response.lower():
-        return 'pediatrica'
-    return 'adulto'
-
-@cl.step(name='Detectando qual o Protocolo da Oftalmologia Adulto...', type='tool')
-async def type_protocol(msg: cl.Message, patient: str):
-
-    print(patient)
-    client = cl.user_session.get('client')
-    protocol_list = ADULT if patient == 'adulto' else CHILD
-    print(protocol_list)
-    response = await client.chat.completions.create(
-        model=MODEL_NAME,
-        messages=[
-            {'content': 'Você é um Assistente da Secretaria de Saúde do Distrito Federal.', 'role': 'system'},
-            {'content': f'Com base no contexto da mensagem, qual dos seguintes protocolos da Oftalmologia Adulto deve ser seguido?\nResponda **somente** o número do protocolo. Se não souber, retorne 0.\n\nMensagem: {msg.content}\n\nProtocolos: {protocol_list}', 'role': 'user'}
-        ], temperature=0
-    )
-
-    return response.choices[0].message.content
-
-@cl.step(name='Identificando se a justificativa atende o protocolo...', type='tool')
-async def protocol_analysis(msg: cl.Message, protocol: str):
+@cl.step(name='Identificando o Área...', type='tool')
+async def area_protocol(msg: cl.Message):
 
     client = cl.user_session.get('client')
     response = await client.chat.completions.create(
         model=MODEL_NAME, 
         messages=[
-            {'content': 'Você é um Assistente da Secretaria de Saúde do Distrito Federal.', 'role': 'system'},
-            {'content': f'Com base no contexto da mensagem, o protocolo do procedimento está sendo seguido de forma adequada?\nResponda **somente** **SIM** ou **NÃO**. Se **NÃO**, retorne a justificativa.\n\nMensagem: {msg.content}\n\nProtocolo: {protocol}', 'role': 'user'}
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''Com base na mensagem, qual dos protocolos deve ser utilizado?
+            Mensagem: {msg.content}
+            Protocolos: {protocol.AREA}
+            Responda **somente** com o **nome** do protocolo.''', 'role': 'user'}
+        ], temperature=0
+    )
+
+    response = response.choices[0].message.content
+    return response
+
+@cl.step(name='Identificando o Protocolo Geral...', type='tool')
+async def general_protocol(area: str, msg: cl.Message):
+
+    client = cl.user_session.get('client')
+    response = await client.chat.completions.create(
+        model=MODEL_NAME, 
+        messages=[
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''Com base na mensagem, qual dos protocolos deve ser utilizado?
+            Mensagem: {msg.content}
+            Protocolos: {getattr(protocol, area.upper())}
+            Responda **somente** com o **nome** do protocolo.''', 'role': 'user'}
+        ], temperature=0
+    )
+
+    response = response.choices[0].message.content
+    return response
+
+@cl.step(name='Identificando o Protocolo Específico...', type='tool')
+async def specific_protocol(general: str, msg: cl.Message):
+
+    client = cl.user_session.get('client')
+    response = await client.chat.completions.create(
+        model=MODEL_NAME, 
+        messages=[
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''Com base na mensagem, qual dos protocolos deve ser utilizado?
+            Mensagem: {msg.content}
+            Protocolos: {getattr(protocol, general.upper().replace(' ', '_'))}
+            Responda **somente** o **número** do protocolo. Se não souber retorne **0**.''', 'role': 'user'}
+        ], temperature=0
+    )
+
+    response = response.choices[0].message.content
+    return response
+
+@cl.step(name='Detectando se a justificativa atende o Protocolo Específico...', type='tool')
+async def protocol_analysis(document: str, msg: cl.Message):
+
+    client = cl.user_session.get('client')
+    response = await client.chat.completions.create(
+        model=MODEL_NAME, 
+        messages=[
+            {'content': f'''Você é um Assistente da Secretaria de Saúde do Distrito Federal.''', 'role': 'system'},
+            {'content': f'''Com base na mensagem e no conteúdo descritivo mínimo do protocolo, o procedimento deve ser aceito?
+            Responda **somente** **SIM** ou **NÃO**. Se **NÃO**, retorne a justificativa.\n
+            Mensagem: {msg.content}\n
+            Protocolo: {document}''', 'role': 'user'}
         ], temperature=0
     )
 
@@ -98,18 +130,30 @@ async def protocol_analysis(msg: cl.Message, protocol: str):
 async def message(msg: cl.Message):
 
     status = await info_checker(msg)
-    if status:
-        
-        patient = await type_extractor(msg)
-        protocol = await type_protocol(msg, patient)
 
-        if protocol.isdigit() and int(protocol) > 0:
-            path = 'regulacao/oftalmologia/' + patient + '/' + protocol + '.txt'
-            lines = read_file (path)
-            justification = await protocol_analysis(msg, lines)
+    if status:
+
+        area = await area_protocol(msg)
+        general = await general_protocol(area, msg)
+        specific = await specific_protocol(general, msg)
+        
+        if specific.isdigit() and int(specific) > 0:
+            
+            path = 'regulacao/' + area.lower() + '/' + general.lower() + '/' + specific + '.txt'
+            document = read_file(path)
+            
+            justification = await protocol_analysis(document, msg)
+            
             if 'sim' in justification.lower():
                 await cl.Message(content=f'Justificativa **suficiente**').send()
             else:
                 await cl.Message(content=f'Justificativa **insuficiente**\n + {justification}').send()    
+        
         else:
+        
             await cl.Message(content=f'Protocolo não identificado. Desculpe-me!').send()    
+
+    else: 
+        await cl.Message(content=f'''Os seguintes campos estão ausêntes: {info_explanation(msg)}.''').send()
+
+        
---
+++ protocol.py
@@ -0,0 +1,78 @@
+AREA = '''- Ginecologia
+- Endocrinologia
+- Cardiologia
+- Ortopedia
+- Neurologia
+- Otorrinolaringologia
+- Dermatologia
+- Reumatologia
+- Psiquiatria
+- Oftalmologia'''
+
+GINECOLOGIA = '''Protocolos de Ginecologia:
+- Infertilidade
+- Ginecologia Adulto
+- Planejamento Reprodutivo
+'''
+
+ENDOCRINOLOGIA = '''Protocolos de Endocrinologia:
+- Endocrinologia Pediátrica
+- Endocrinologia Adulto
+'''
+
+CARDIOLOGIA = '''Protocolo de Cardiologia:
+- Cardiologia Adulto
+'''
+
+ORTOPEDIA = '''Protocolo de Ortopedia:
+- Ortopedia Adulto
+'''
+
+NEUROLOGIA = '''
+- Neurologia Adulto
+- Neurologia Pediátrica
+'''
+
+OTORRINOLARINGOLOGIA = '''
+- Otorrinolaringologia Adulto
+'''
+
+DERMATOLOGIA = '''
+- Dermatologia
+'''
+
+REUMATOLOGIA = '''
+- Reumatologia
+'''
+
+PSIQUIATRIA = '''
+Saúde Mental Infantil
+Psiquiatria Adulto
+'''
+
+OFTALMOLOGIA = '''
+Oftalmologia Adulto
+Oftalmologia Pediátrica
+'''
+
+OFTALMOLOGIA_ADULTO = '''
+Protocolo 1 – Distúrbios de Refração ou Acomodação
+Protocolo 2 – Catarata
+Protocolo 3 – Retinopatia ou Outras Doenças de Retina
+Protocolo 4 – Estrabismo
+Protocolo 5 – Doenças das Pálpebras, Vias Lacrimais e Órbita
+Protocolo 6 – Glaucoma
+Protocolo 7 – Doenças da Córnea e da Superfície Ocular
+Protocolo 8 – Uveítes
+'''
+
+OFTALMOLOGIA_PEDIÁTRICA='''
+Protocolo 1 – Distúrbios de Refração ou Acomodação
+Protocolo 2 – Estrabismo
+Protocolo 3 – Teste do Olhinho Alterado ou Leucocoria
+Protocolo 4 – Catarata Congênita ou Infantil
+Protocolo 5 – Glaucoma Congênito ou Infantil
+Protocolo 6 – Doenças das Pálpebras, Vias Lacrimais e Órbita
+Protocolo 7 – Uveítes
+Protocolo 8 – Retinopatia da Prematuridade
+'''
