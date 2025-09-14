5de9fc862cde1abf3c8f04581173c452e456a658
lpfgarcia <lpfgarcia@gmail.com>
2025-09-14 16:28:37 -0300

    update areas

--- protocol.py
+++ protocol.py
@@ -1,78 +1,29 @@
 AREA = '''- Ginecologia
-- Endocrinologia
-- Cardiologia
-- Ortopedia
-- Neurologia
-- Otorrinolaringologia
-- Dermatologia
-- Reumatologia
-- Psiquiatria
-- Oftalmologia'''
+- Ortopedia'''
 
 GINECOLOGIA = '''Protocolos de Ginecologia:
-- Infertilidade
 - Ginecologia Adulto
-- Planejamento Reprodutivo
-'''
-
-ENDOCRINOLOGIA = '''Protocolos de Endocrinologia:
-- Endocrinologia Pediátrica
-- Endocrinologia Adulto
-'''
-
-CARDIOLOGIA = '''Protocolo de Cardiologia:
-- Cardiologia Adulto
 '''
 
 ORTOPEDIA = '''Protocolo de Ortopedia:
 - Ortopedia Adulto
 '''
 
-NEUROLOGIA = '''
-- Neurologia Adulto
-- Neurologia Pediátrica
+GINECOLOGIA_ADULTO = '''
+Protocolo 1 – Sangramento Uterino Anormal
+Protocolo 2 – Pólipo Endometrial
+Protocolo 3 – Massa Anexial
+Protocolo 4 – Miomatose
+Protocolo 5 – Anormalidades da Estática Pélvica
+Protocolo 6 – Incontinência Urinária
+Protocolo 7 – Dor Pélvica Crônica, Adenomiose e Endometriose
+Protocolo 8 – Climatério
+Protocolo 9 – Neoplasia de Endométrio
+Protocolo 10 – Neoplasia do Colo Uterino
+Protocolo 11 – Amenorreia
+Protocolo 12 – Condiloma Acuminado, Verrugas Virais e Molusco Contagioso
+Protocolo 13 – Lesões Vulvares
 '''
 
-OTORRINOLARINGOLOGIA = '''
-- Otorrinolaringologia Adulto
-'''
-
-DERMATOLOGIA = '''
-- Dermatologia
-'''
-
-REUMATOLOGIA = '''
-- Reumatologia
-'''
-
-PSIQUIATRIA = '''
-Saúde Mental Infantil
-Psiquiatria Adulto
-'''
-
-OFTALMOLOGIA = '''
-Oftalmologia Adulto
-Oftalmologia Pediátrica
-'''
-
-OFTALMOLOGIA_ADULTO = '''
-Protocolo 1 – Distúrbios de Refração ou Acomodação
-Protocolo 2 – Catarata
-Protocolo 3 – Retinopatia ou Outras Doenças de Retina
-Protocolo 4 – Estrabismo
-Protocolo 5 – Doenças das Pálpebras, Vias Lacrimais e Órbita
-Protocolo 6 – Glaucoma
-Protocolo 7 – Doenças da Córnea e da Superfície Ocular
-Protocolo 8 – Uveítes
-'''
-
-OFTALMOLOGIA_PEDIÁTRICA='''
-Protocolo 1 – Distúrbios de Refração ou Acomodação
-Protocolo 2 – Estrabismo
-Protocolo 3 – Teste do Olhinho Alterado ou Leucocoria
-Protocolo 4 – Catarata Congênita ou Infantil
-Protocolo 5 – Glaucoma Congênito ou Infantil
-Protocolo 6 – Doenças das Pálpebras, Vias Lacrimais e Órbita
-Protocolo 7 – Uveítes
-Protocolo 8 – Retinopatia da Prematuridade
+ORTOPEDIA_ADULTO = '''
 '''
